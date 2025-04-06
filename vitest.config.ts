6e2656f7da60ffc749a539922a0a87cd9d47a217
namidapoo <83203852+ve1997@users.noreply.github.com>
2025-04-07 01:47:56 +0900

    feat: 都道府県を地域ごとにグルーピングする関数を追加

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
 	test: {
 		coverage: {
 			provider: "v8",
-			include: ["{app,components,stories}/**/*"],
+			include: ["{app,components}/**/*"],
 			exclude: [
 				...coverageConfigDefaults.exclude,
 				"**/next.config.ts",
